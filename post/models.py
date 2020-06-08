8d899aeba0cc827db09005f76a51650546befc52
Julia <riyamtechcampus@gmail.com>
2020-06-08 13:38:57 +0300

    add new post app to the project

---
+++ post/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50, blank=True, null=True)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(default=timezone.now())
+    image = models.ImageField(upload_to='post-img')
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for Post."""
+
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        """Unicode representation of Post."""
+        pass
